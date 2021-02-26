09eb3f82b46309c8ac842035e9f6e6ec62ff0797
Flávio Jussiê <flaviojussie@users.noreply.github.com>
2021-02-26 12:06:12 -0300

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,23 @@
+const translations = {
+    en: {
+        title: 'Attendance for Google Meet',
+        version: 'Version',
+        attribution: 'By Tyler Lin and Adit Bala',
+        exporting: 'Exporting',
+        areYouSure: 'Are you sure you want to delete the class'
+    },
+    fr: {
+        title: 'Appel pour Google Meet',
+        version: 'Version',
+        attribution: 'De Tyler Lin et Adit Bala',
+        exporting: 'Exportant',
+        areYouSure: 'idk bruh'
+    },
+    pt_br: {
+        title: 'Frequência para Google Meet',
+        version: 'Versão',
+        attribution: 'Project original By Tyler Lin and Adit Bala',
+        exporting: 'Exportando',
+        areYouSure: 'Tem certeza que deseja deletar a classe'
+    },
+}
